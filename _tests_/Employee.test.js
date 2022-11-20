8ecbed8621742bad3adbe8212f39d9221757f341
sensational.mack <mackn478@gmail.com>
2022-11-20 17:22:35 -0500

    update employee test route

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,5 +1,8 @@
 const Employee = require("../lib/Employee");
 test("this is required testing for Employee portion", () => {
   const employee = new Employee("Mack", 1194, "letsgetcoding@gmail.com");
-  expext(employee.name).toEqual;
+  expect(employee.name).toEqual(expect.any(string));
+  expect(employee.id).toEqual(expect.any(number));
+  expect(employee.email).toContain(expect.any("@"));
 });
+
